8e26a7d411a6948843c733c193113239020dbcab
maxthegray <maximilian.i.reich@gmail.com>
2025-10-07 16:08:14 -0400

    starting pedropathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -29,6 +29,15 @@ import org.firstinspires.ftc.teamcode.Constants;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.configurables.annotations.IgnoreConfigurable;
+import com.bylazar.configurables.PanelsConfigurables;
+import com.bylazar.field.FieldManager;
+import com.bylazar.field.PanelsField;
+import com.bylazar.field.Style;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+
 /**
  * This is the Tuning class. It contains a selection menu for various tuning OpModes.
  *
@@ -1183,30 +1192,30 @@ class Circle extends OpMode {
 /**
  * This is the Drawing class. It handles the drawing of stuff on Panels Dashboard, like the robot.
  *
+ /**
+ * This is the Drawing class. It handles the drawing of stuff on Panels Dashboard, like the robot.
+ *
  * @author Lazar - 19234
  * @version 1.1, 5/19/2025
  */
 class Drawing {
     public static final double ROBOT_RADIUS = 9; // woah
     private static final FieldManager panelsField = PanelsField.INSTANCE.getField();
-
     private static final Style robotLook = new Style(
-            "", "#3F51B5", 0.75
+            "", "#3F51B5", 0.0
     );
     private static final Style historyLook = new Style(
-            "", "#4CAF50", 0.75
+            "", "#4CAF50", 0.0
     );
-
     /**
      * This prepares Panels Field for using Pedro Offsets
      */
     public static void init() {
         panelsField.setOffsets(PanelsField.INSTANCE.getPresets().getPEDRO_PATHING());
     }
-
     /**
      * This draws everything that will be used in the Follower's telemetryDebug() method. This takes
-     * a Follower as an input, so an instance of the DashbaordDrawingHandler class is not needed.
+     * a Follower as an input, so an instance of the DashboardDrawingHandler class is not needed.
      *
      * @param follower Pedro Follower instance.
      */
@@ -1218,10 +1227,8 @@ class Drawing {
         }
         drawPoseHistory(follower.getPoseHistory(), historyLook);
         drawRobot(follower.getPose(), historyLook);
-
         sendPacket();
     }
-
     /**
      * This draws a robot at a specified Pose with a specified
      * look. The heading is represented as a line.
@@ -1233,21 +1240,17 @@ class Drawing {
         if (pose == null || Double.isNaN(pose.getX()) || Double.isNaN(pose.getY()) || Double.isNaN(pose.getHeading())) {
             return;
         }
-
         panelsField.setStyle(style);
         panelsField.moveCursor(pose.getX(), pose.getY());
         panelsField.circle(ROBOT_RADIUS);
-
         Vector v = pose.getHeadingAsUnitVector();
         v.setMagnitude(v.getMagnitude() * ROBOT_RADIUS);
         double x1 = pose.getX() + v.getXComponent() / 2, y1 = pose.getY() + v.getYComponent() / 2;
         double x2 = pose.getX() + v.getXComponent(), y2 = pose.getY() + v.getYComponent();
-
         panelsField.setStyle(style);
         panelsField.moveCursor(x1, y1);
         panelsField.line(x2, y2);
     }
-
     /**
      * This draws a robot at a specified Pose. The heading is represented as a line.
      *
@@ -1256,7 +1259,6 @@ class Drawing {
     public static void drawRobot(Pose pose) {
         drawRobot(pose, robotLook);
     }
-
     /**
      * This draws a Path with a specified look.
      *
@@ -1265,7 +1267,6 @@ class Drawing {
      */
     public static void drawPath(Path path, Style style) {
         double[][] points = path.getPanelsDrawingPoints();
-
         for (int i = 0; i < points[0].length; i++) {
             for (int j = 0; j < points.length; j++) {
                 if (Double.isNaN(points[j][i])) {
@@ -1273,12 +1274,10 @@ class Drawing {
                 }
             }
         }
-
         panelsField.setStyle(style);
         panelsField.moveCursor(points[0][0], points[0][1]);
         panelsField.line(points[1][0], points[1][1]);
     }
-
     /**
      * This draws all the Paths in a PathChain with a
      * specified look.
@@ -1291,7 +1290,6 @@ class Drawing {
             drawPath(pathChain.getPath(i), style);
         }
     }
-
     /**
      * This draws the pose history of the robot.
      *
@@ -1300,15 +1298,12 @@ class Drawing {
      */
     public static void drawPoseHistory(PoseHistory poseTracker, Style style) {
         panelsField.setStyle(style);
-
         int size = poseTracker.getXPositionsArray().length;
         for (int i = 0; i < size - 1; i++) {
-
             panelsField.moveCursor(poseTracker.getXPositionsArray()[i], poseTracker.getYPositionsArray()[i]);
             panelsField.line(poseTracker.getXPositionsArray()[i + 1], poseTracker.getYPositionsArray()[i + 1]);
         }
     }
-
     /**
      * This draws the pose history of the robot.
      *
@@ -1317,7 +1312,6 @@ class Drawing {
     public static void drawPoseHistory(PoseHistory poseTracker) {
         drawPoseHistory(poseTracker, historyLook);
     }
-
     /**
      * This tries to send the current packet to FTControl Panels.
      */
