6474398fae7449b334923189abc16ea741b202ac
aaravramnath <aaravramnath@gmail.com>
2025-11-06 16:37:57 -0500

    maxie can run it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Light.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Light.java
@@ -1,26 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
+import static android.os.SystemClock.sleep;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 @TeleOp(name = "Light", group = "Testing")
 public class Light extends OpMode {
     Servo light;
+    double position;
 
+    @Override
     public void init() {
         light = hardwareMap.get(Servo.class, "light");
+        position = 0;
     }
 
+    @Override
     public void loop() {
+        light.setPosition(position);
         telemetry.addData("Light Position", light.getPosition());
 
-        if (gamepad1.cross) {
-            light.setPosition(1.0); // Turn on the light
+
+        if (gamepad1.dpad_up) {
+            position +=.05;
+            sleep(100);
         }
-        if (gamepad1.square) {
-            light.setPosition(0);
+        if (gamepad1.dpad_down) {
+            position -=.05;
+            sleep(100);
         }
-        telemetry.update();
+
 
     }
 
