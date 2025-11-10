42eb82eecabd28353a95dd4c727edf96ae519ebc
maxthegray <maximilian.i.reich@gmail.com>
2025-11-10 12:28:58 -0500

    light detector for testing when we get pipelines figured out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
@@ -2,25 +2,35 @@ package org.firstinspires.ftc.teamcode;
 
 import static android.os.SystemClock.sleep;
 
+import com.pedropathing.follower.Follower;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+
 @TeleOp(name = "detector", group = "Testing")
 public class Detector extends OpMode {
     Servo light;
     double position;
     Limelight3A limelight;
     LLResult result = limelight.getLatestResult();
+    private Follower follower;
+    private double adjustmentCoeficcient;
 
     int greenPurple = 0; //0 is green 1 is purple
 
     @Override
     public void init() {
+        follower = Constants.createFollower(hardwareMap);
+
         light = hardwareMap.get(Servo.class, "light");
         position = 0;
         limelight = hardwareMap.get(Limelight3A.class, "slur");
@@ -33,14 +43,27 @@ public class Detector extends OpMode {
         if (greenPurple == 0) {
             if (result.isValid()) {
                 light.setPosition(.475);
+                adjustmentCoeficcient = result.getTx()/360;
+                telemetry.addData("adjustment", adjustmentCoeficcient);
                 telemetry.addData("x diff", result.getTx());
-            }
-        } else if (greenPurple == 1) {
+            } else {adjustmentCoeficcient = 0;}
+
+
+    } else if (greenPurple == 1) {
             if (result.isValid()) {
                 light.setPosition(.722);
+                adjustmentCoeficcient = result.getTx()/360;
+                telemetry.addData("adjustment", adjustmentCoeficcient);
                 telemetry.addData("x diff", result.getTx());
-            }
+            } else {adjustmentCoeficcient = 0;}
         }
+        follower.setTeleOpDrive(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x,
+                -gamepad1.right_stick_x + adjustmentCoeficcient,
+                true // Robot Centric
+        );
+
         telemetry.update();
 
         if (gamepad1.dpad_up) {
