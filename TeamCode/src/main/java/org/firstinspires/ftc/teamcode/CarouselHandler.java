6424b915be38a800950b9dd8b391064b5620aff0
aaravramnath <aaravramnath@gmail.com>
2025-11-20 16:56:03 -0500

    carousel logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarouselHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarouselHandler.java
@@ -4,16 +4,19 @@ import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @TeleOp(name = "Carousel Handler", group = "TeleOp")
 public class CarouselHandler extends LinearOpMode {
 
     private RevColorSensorV3 colorSensor;
 
-    private int[] slots = {0, 0, 0};
-    private int currentSlot = 0;
-    private int rotationDegrees = currentSlot * 120;
-    private double threshold = 0.1;
+    private List <Integer> slots = new ArrayList<>(Arrays.asList(0,0,0));
+    private int rotationDegrees =0;
+    private double threshold = 80;
 
     @Override
     public void runOpMode() {
@@ -42,16 +45,18 @@ public class CarouselHandler extends LinearOpMode {
     }
 
     public void rotateToNext() {
-        currentSlot = (currentSlot + 1) % 3;
+        Collections.rotate(slots, -1);
+        rotationDegrees-=120;
     }
 
     public void rotateToPrevious() {
-        currentSlot = (currentSlot - 1 + 3) % 3;
+        Collections.rotate(slots,1);
+        rotationDegrees+=120;
     }
 
     private void assignColorToCurrentSlot() {
         int color = detectColor();
-        slots[currentSlot] = color;
+        slots.set(0,color);
     }
 
     private int detectColor() {
@@ -59,8 +64,8 @@ public class CarouselHandler extends LinearOpMode {
         int green = colorSensor.green();
         int blue = colorSensor.blue();
 
-        if (red > threshold || green > threshold) {
-            if (green > red) {
+        if (blue > threshold || green > threshold) {
+            if (green > blue) {
                 return 2;
             } else {
                 return 1;
@@ -70,8 +75,8 @@ public class CarouselHandler extends LinearOpMode {
     }
 
     private void displaySlots() {
-        telemetry.addData("slots", "[%d, %d, %d]", slots[0], slots[1], slots[2]);
-        telemetry.addData("intake slot", currentSlot);
+        telemetry.addData("slots", "[%d, %d, %d]", slots.get(0), slots.get(1), slots.get(2));
+        telemetry.addData("intake slot", 0);
         telemetry.addData("rotation (degrees)", rotationDegrees);
         telemetry.addData("color sensor readings", "R: %d, G: %d, B: %d", colorSensor.red(), colorSensor.green(), colorSensor.blue());
         telemetry.update();
