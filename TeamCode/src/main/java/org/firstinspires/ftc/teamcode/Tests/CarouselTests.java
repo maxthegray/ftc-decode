6b84f8a8649fc362ebb484939d889edbc1f35eeb
maxthegray <maximilian.i.reich@gmail.com>
2025-12-11 18:44:46 -0500

    finished carousel and flicker test code with check for whether carousel is at good state

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/CarouselTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/CarouselTests.java
@@ -8,60 +8,129 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.R;
-
 @TeleOp(name = "CarouselTest", group = "Testing")
 public class CarouselTests extends OpMode {
-    DcMotor carouselMotor;
-    double motorPosition;
-    int targetPosition = 0;
-    int ticksPerRevolution = 6700/4;
-    int position1 = 0;
-    int position2 = ticksPerRevolution / 3;
-    int position3 = (ticksPerRevolution / 3) * 2;
 
+    // Carousel constants and variables
+    private DcMotor carouselMotor;
+    private int targetPosition = 0;
+    private static final int Ticks_per_rev = 6700 / 4;
+    private static final int POSITION_1 = 0;
+    private static final int POSITION_2 = Ticks_per_rev / 3;
+    private static final int POSITION_3 = (Ticks_per_rev / 3) * 2;
+    private static final double power = 0.6;
+    private static final int POSITION_TOLERANCE = 5;
+
+    // flicker constants and variables
+    private Servo flickerServo;
+    private double flickerPosition = 0;
+    private static final double down = 0;
+    private static final double up = 0.4;
+    private static final long delay = 50; //ms
+
+    // limit switches
     private DigitalChannel leftFinLimit;
     private DigitalChannel rightFinLimit;
 
     @Override
     public void init() {
-       carouselMotor = hardwareMap.dcMotor.get("carouselMotor");
-       carouselMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       carouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-       leftFinLimit = hardwareMap.get(DigitalChannel.class, "leftFin");
-       leftFinLimit.setMode(DigitalChannel.Mode.INPUT);
-
-       rightFinLimit = hardwareMap.get(DigitalChannel.class, "rightFin");
-       rightFinLimit.setMode(DigitalChannel.Mode.INPUT);
+        initCarousel();
+        initFlicker();
+        initLimitSwitches();
     }
 
     @Override
     public void loop() {
-        motorPosition = carouselMotor.getCurrentPosition();
+        handleCarouselInput();
+        updateCarousel();
+
+        handleFlickerInput();
+        updateFlicker();
+
+        updateTelemetry();
+    }
+
+    // init methods
+
+    private void initCarousel() {
+        carouselMotor = hardwareMap.dcMotor.get("carouselMotor");
+        carouselMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        carouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private void initFlicker() {
+        flickerServo = hardwareMap.get(Servo.class, "flickServo");
+        flickerPosition = down;
+    }
+
+    private void initLimitSwitches() {
+        leftFinLimit = hardwareMap.get(DigitalChannel.class, "leftFin");
+        leftFinLimit.setMode(DigitalChannel.Mode.INPUT);
+
+        rightFinLimit = hardwareMap.get(DigitalChannel.class, "rightFin");
+        rightFinLimit.setMode(DigitalChannel.Mode.INPUT);
+    }
+
+    // carousel methods
+
+    private void handleCarouselInput() {
         if (gamepad1.square) {
-            targetPosition = position1;
+            targetPosition = POSITION_1;
+        } else if (gamepad1.triangle) {
+            targetPosition = POSITION_2;
+        } else if (gamepad1.circle) {
+            targetPosition = POSITION_3;
         }
-        if (gamepad1.triangle) {
-            targetPosition = position2;
-        }
-        if (gamepad1.circle) {
-            targetPosition = position3;
-        }
+    }
+
+    private void updateCarousel() {
         carouselMotor.setTargetPosition(targetPosition);
-
         carouselMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        carouselMotor.setPower(0.6);
-//          carouselMotor.setPower(gamepad1.left_stick_x);
+        carouselMotor.setPower(power);
+    }
 
+    private boolean isCarouselSettled() {
+        int error = Math.abs(carouselMotor.getCurrentPosition() - targetPosition);
+        return error <= POSITION_TOLERANCE;
+    }
+
+    // flicker methods
+
+    private void handleFlickerInput() {
+        if (!isCarouselSettled()) {
+            return;
+        }
+
+        if (gamepad1.dpad_up) {
+            setFlickerPosition(up);
+        } else if (gamepad1.dpad_down) {
+            setFlickerPosition(down);
+        }
+    }
+
+    private void setFlickerPosition(double position) {
+        flickerPosition = position;
+        sleep(delay);
+    }
+
+    private void updateFlicker() {
+        flickerServo.setPosition(flickerPosition);
+    }
+
+    // telemetry
+
+    private void updateTelemetry() {
+        telemetry.addData("Servo Position", flickerPosition);
         telemetry.addData("Carousel Motor Power", carouselMotor.getPower());
         telemetry.addData("Carousel Motor Position", carouselMotor.getCurrentPosition());
-
-        telemetry.addData("Left Fin", leftFinLimit.getState() ? "P" : "MP");
-        telemetry.addData("Right Fin", rightFinLimit.getState() ? "P" : "NP");
-
+        telemetry.addData("Carousel Target", targetPosition);
+        telemetry.addData("Carousel Settled", isCarouselSettled() ? "Y" : "N");
+        telemetry.addData("Left Fin", getLimitSwitchStatus(leftFinLimit));
+        telemetry.addData("Right Fin", getLimitSwitchStatus(rightFinLimit));
         telemetry.update();
     }
 
+    private String getLimitSwitchStatus(DigitalChannel limitSwitch) {
+        return limitSwitch.getState() ? "P" : "NP";
+    }
 }
-
