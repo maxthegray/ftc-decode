5dc41abda45bcfcef36f52fe7f7b9025df78b2d4
drhgitcs <68492840+drhgitcs@users.noreply.github.com>
2026-01-31 14:20:02 -0500

    Update XdriveTrain.java
    Major update including turn while driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/threaded/New/XdriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/threaded/New/XdriveTrain.java
@@ -8,17 +8,20 @@ import static java.lang.Math.abs;
 import static java.lang.Math.PI;
 import static java.lang.Math.sin;
 import static java.lang.Math.cos;
+import static java.lang.Math.abs;
 
 public class XdriveTrain extends Thread {
     
     private final double PI_4TH = PI/4.0;
-    private final double PI_3RD = PI/3.0;
+    //private final double PI_3RD = PI/3.0;
     private final double KP = 0.35;
+    private final double MOTOR_BUFFER = 0.98;
     
-    private volatile float magnitude, angle, turn, driveThreshold;
+    private float magnitude, angle, turn, driveThreshold;
+    private double frmPower, flmPower, brmPower, blmPower, maxPower;
+    private double x, y, f, direction; // deltaDirection; removed with isTurning
+    private boolean isTurning, clipPower;
 
-    private double x, y, f, direction, deltaDirection;
-    
     private final float TURN_THRESHOLD = 0.05f;
     
     private HardwareMap hardwareMap;
@@ -31,10 +34,19 @@ public class XdriveTrain extends Thread {
         this.magnitude = 0.0f;
         this.angle = 0.0f;
         this.turn = 0.0f;
+
+        this.frmPower = 0.0;
+        this.flmPower = 0.0;
+        this.brmPower = 0.0;
+        this.blmPower = 0.0;
+        this.maxPower = 1.0;
+
+        this.isTurning = false;
+        this.clipPower = false;
         
         this.direction = direction;
-        deltaDirection = 0.0;
-        f = 0.0;
+        //deltaDirection = 0.0;
+        this.f = 0.0;
         
         this.driveThreshold = threshold;
         
@@ -51,10 +63,10 @@ public class XdriveTrain extends Thread {
         while (!state.shouldKillThreads()) {
 
             // need to force +/- PI
-            direction += deltaDirection;
+            //direction += deltaDirection;
 
-            x = (double) magnitude * cos(angle - direction);
-            y = (double) magnitude * sin(angle - direction);
+            x = (double) magnitude * cos((double) angle - direction);
+            y = (double) magnitude * sin((double) angle - direction);
 
             if (magnitude < driveThreshold) {
 
@@ -66,39 +78,52 @@ public class XdriveTrain extends Thread {
 
                 // case: robot is turning only
                 else {
+                    isTurning = true;
                     // power the wheels to turn the robot
                     frontRightMotor.setPower(turn);
                     frontLeftMotor.setPower(turn);
                     backRightMotor.setPower(turn);
                     backLeftMotor.setPower(turn);
-                    direction = state.getHeading();
+                    //direction = state.getHeading();
                 }
 
             } else {
 
-                f = direction - state.getHeading();
-
-                if (abs(f) < PI_4TH) {
-                    f = KP * f / PI_4TH;
-                } else {
-                    f = KP;
+                // case: drive the robot without turning
+                if (abs(turn) < TURN_THRESHOLD) {
+                    f = direction - state.getHeading();
+                    if (abs(f) < PI_4TH) {
+                        f = KP * f / PI_4TH;
+                    } else {
+                        f = KP;
+                    }
+                    powerWheels(x, y, f);
+                    //frontRightMotor.setPower(x + y + f);
+                    //frontLeftMotor.setPower(-x + y + f);
+                    //backRightMotor.setPower(x - y + f);
+                    //backLeftMotor.setPower(-x - y + f);
                 }
 
-                // need to replace wit motor method(x,y,f) that clips power
-                frontRightMotor.setPower(x + y + f);
-                frontLeftMotor.setPower(-x + y + f);
-                backRightMotor.setPower(x - y + f);
-                backLeftMotor.setPower(-x - y + f);
-
+                // case: drive the robot while turning
+                else {
+                    isTurning = true;
+                    powerWheels(x, y, turn);
+                }
             }  // end if-else structure
 
             try {
-                Thread.sleep(8l);
+                Thread.sleep(4l);   // was originally 8l, now matching otos update timing
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt(); // Restore the interrupt flag
-            }
-        }
+            }  // end try-catch
 
+            // update direction from latest heading
+            if (isTurning) {
+                direction = state.getHeading();
+                isTurning = false;
+            }  // end if
+
+        }  // end while
 
     }  // end threaded method
     
@@ -120,6 +145,46 @@ public class XdriveTrain extends Thread {
         backLeftMotor.setPower(0.0);
         
     }  // end method setPowerZero
+
+
+    private void powerWheels(double x, double y, double t) {
+
+        frmPower = x + y + t;
+        flmPower = -x + y + t;
+        brmPower = x - y + t;
+        blmPower = -x - y + t;
+
+        maxPower = 1.0;
+        clipPower = false;
+
+        if (abs(frmPower) > maxPower) {
+            maxPower = abs(frmPower);
+            clipPower = true;
+        }
+        if (abs(flmPower) > maxPower) {
+            maxPower = abs(flmPower);
+            clipPower = true;
+        }
+        if (abs(brmPower) > maxPower) {
+            maxPower = abs(brmPower);
+            clipPower = true;
+        }
+        if (abs(blmPower) > maxPower) {
+            maxPower = abs(blmPower);
+            clipPower = true;
+        } // end if stack
+
+        frmPower = MOTOR_BUFFER * frmPower / maxPower;
+        flmPower = MOTOR_BUFFER * flmPower / maxPower;
+        brmPower = MOTOR_BUFFER * brmPower / maxPower;
+        blmPower = MOTOR_BUFFER * blmPower / maxPower;
+
+        frontRightMotor.setPower(frmPower);
+        frontLeftMotor.setPower(flmPower);
+        backRightMotor.setPower(brmPower);
+        backLeftMotor.setPower(blmPower);
+
+    }  // end method powerWheels
     
     
     private void initializeMotors() {
@@ -150,8 +215,6 @@ public class XdriveTrain extends Thread {
         backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         
     }  // end method initialize
-    
-    
 
     
 }  // end class
